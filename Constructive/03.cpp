80d5eb2b5271525fa34903a7cdf3e6a0573283e1
Dhairya Singla <97469815+dhairyasingla09@users.noreply.github.com>
2023-03-24 20:48:06 +0530

    Create 03.cpp

---
+++ Constructive/03.cpp
@@ -0,0 +1,20 @@
+//dhairyasingla09
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[5][5];
+    int x,y;
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            cin>>arr[i][j];
+            if(arr[i][j]==1){
+                x=i;
+                y=j;
+            }
+        }
+    }
+
+    cout<<abs(2-x) + abs(2-y)<<endl;
+    return 0;
+}
